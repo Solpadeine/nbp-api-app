7df4b17654e8404b3852d2f089caff7232afcd98
Solpadeine <patryk@gustomania.pl>
2022-12-07 15:36:37 +0100

    Create all records from the api fetch in the database Sync method is going to loop through all records and determine whether to update, create or leave the iterated object

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -3,15 +3,15 @@
 namespace App\Http\Controllers;
 
 use App\Services\CurrencyService;
-use Illuminate\Http\Request;
 
 class CurrencyController extends Controller
 {
     public function __construct(private CurrencyService $currencyService)
     {
     }
+
     public function sync()
     {
-        return $this->currencyService->fetch();
+        $this->currencyService->sync();
     }
 }
--- app/Services/CurrencyService.php
+++ app/Services/CurrencyService.php
@@ -2,14 +2,33 @@
 
 namespace App\Services;
 
+use App\Models\Currency;
 use Illuminate\Support\Facades\Http;
 
 class CurrencyService
 {
+    public function sync()
+    {
+        $currencies = $this->fetch();
+
+        foreach ($currencies[0]->rates as $item) {
+            $this->create($item);
+        }
+    }
+
     public function fetch()
     {
         $response = Http::get('http://api.nbp.pl/api/exchangerates/tables/a/');
 
         return json_decode($response->body());
+    }
+
+    public function create($item)
+    {
+        Currency::create([
+            'name' => $item->currency,
+            'currency_code' => $item->code,
+            'exchange_rate' => str_replace('.', '', $item->mid)
+        ]);
     }
 }
